837b335cc8604dc09d07fc8a01d514ad5e000ab8
Issac Gerges <igerges@atlassian.com>
2017-09-14 11:20:08 -0500

    add error case for invisible title bar

--- main.js
+++ main.js
@@ -13,7 +13,12 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+      vibrancy: 'light',
+      titleBarStyle: 'hiddenInset',
+      width: 800,
+      height: 600
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({

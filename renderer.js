0da703ecbaeecddc588a3bfca908172cffbd0542
Issac Gerges <igerges@atlassian.com>
2017-10-26 15:42:28 -0500

    add example

--- renderer.js
+++ renderer.js
@@ -1,3 +1,8 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+document.querySelector('#notify').addEventListener('click', () => {
+    let t = new Notification("Here I am", {body: '👋'})
+    t.onclick = (e) => {
+        console.log(e)
+        let log = document.querySelector('#log');
+        log.innerHTML += 'notification clicked <br/>';
+    }
+});
